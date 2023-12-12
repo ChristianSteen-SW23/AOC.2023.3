434c0387cd4628f66e10dcfa07ab34b7802cc8e2
ChristianSteen-SW23 <cniel23@student.aau.dk>
2023-12-12 21:18:06 +0100

    Check for spceial char

--- AOC2023-3/Program.cs
+++ AOC2023-3/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, test");
+﻿using System;
+using System.IO;
+
+namespace AOC20233
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "datafile.txt";
+
+            string[] lines = ReadFile(filePath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+
+            foreach (string line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                        case '.':
+                            break;
+                        default:
+                            Console.Write(line[i] + " ");
+                            break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.ReadLine();
+        }
+        static string[] ReadFile(string filePath)
+        {
+            try
+            {
+                int lineCount = 0;
+                // Finds the amount of lines in a the data file
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (sr.ReadLine() != null)
+                    {
+                        lineCount++;
+                    }
+                }
+
+                Console.WriteLine("Line count is: " + lineCount);
+
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    // Sets up the array to storge the data
+                    string[] lines = new string[lineCount];
+
+                    // Makes a forloop to scan in all the data
+                    for (int i = 0; i < lineCount; i++)
+                    {
+                        lines[i] = sr.ReadLine();
+                    }
+                    /*foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }*/
+                    return lines;
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Did not find the file");
+                Environment.Exit(-1);
+                return null;
+            }
+        }
+    }
+}
