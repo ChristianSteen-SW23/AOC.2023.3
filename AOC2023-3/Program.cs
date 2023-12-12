ab9fd31181f26e2037161fe295de47d3f97b7a40
ChristianSteen-SW23 <cniel23@student.aau.dk>
2023-12-12 22:17:20 +0100

    Gold Start

--- AOC2023-3/Program.cs
+++ AOC2023-3/Program.cs
@@ -7,39 +7,55 @@ namespace AOC20233
     {
         static void Main(string[] args)
         {
-            string filePath = "datafile.txt";
+            string filePath = "datafilebig.txt";
 
             string[] lines = ReadFile(filePath);
-            foreach (string line in lines)
-            {
-                Console.WriteLine(line);
-            }
 
-
-            foreach (string line in lines)
+            int counter = 0;
+            for (int lineC = 0; lineC  < lines.Length; lineC ++)
             {
-                for (int i = 0; i < line.Length; i++)
+                Console.WriteLine(lines[lineC]);
+                for (int index = 0; index < lines[lineC].Length; index++)
                 {
-                    switch (line[i])
+                    Console.WriteLine(lineC+" "+index);
+                    if (!CheckForSpecialChar(lines[lineC][index])) continue;
+
+                    if (CheckForNumberChar(lines[lineC-1][index]))
                     {
-                        case '1':
-                        case '2':
-                        case '3':
-                        case '4':
-                        case '5':
-                        case '6':
-                        case '7':
-                        case '8':
-                        case '9':
-                        case '.':
-                            break;
-                        default:
-                            Console.Write(line[i] + " ");
-                            break;
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index));
+                    } else
+                    {
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index-1));
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index+1));
                     }
+
+                    if (CheckForNumberChar(lines[lineC + 1][index]))
+                    {
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index));
+                    }
+                    else
+                    {
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index - 1));
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index + 1));
+                    }
+
+                    if (CheckForNumberChar(lines[lineC][index - 1])){
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC, index-1));
+                    }
+                    if (CheckForNumberChar(lines[lineC][index + 1]))
+                    {
+                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC, index + 1));
+                    }
+
+                    
+
+
+                    //string number = FindNumberFromChar(lines, lineC - 1, index - 1);
+                    //Console.WriteLine(number);
+                    //Console.Write(lines[lineC][index]);
                 }
-                Console.WriteLine();
             }
+            Console.WriteLine("\n\n"+counter);
 
 
 
@@ -85,6 +101,79 @@ namespace AOC20233
                 Environment.Exit(-1);
                 return null;
             }
+        }
+
+        static bool CheckForSpecialChar(char ch)
+        {
+            switch (ch)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '.':
+                    return false;
+                    break;
+                default:
+                    return true;
+                    break;
+            }
+        }
+        static bool CheckForNumberChar(char ch)
+        {
+            switch (ch)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    return true;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+
+        static string FindNumberFromChar(string[] lines, int line, int ch)
+        {
+            if (!CheckForNumberChar(lines[line][ch])) return "0";
+
+            int leftCount = 0;
+            for (int h = 0; ch - h >= 0 && ch + h <= 139 && CheckForNumberChar(lines[line][ch - h]); h++)
+            {
+                leftCount = h;
+            }
+
+            int rightCount = 0;
+            for (int h = 0; ch - h >= 0 && ch + h <= 139 && CheckForNumberChar(lines[line][ch + h]); h++)
+            {
+                rightCount = h;
+            }
+
+            string number = lines[line][ch].ToString();
+            for (int h = 1; h < leftCount + 1; h++)
+            {
+                number = string.Concat(lines[line][ch - h].ToString(), number);
+            }
+            for (int h = 1; h < rightCount + 1; h++)
+            {
+                number = string.Concat(number, lines[line][ch + h].ToString());
+            }
+
+            return number;
         }
     }
 }
