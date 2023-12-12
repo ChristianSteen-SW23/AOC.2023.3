6fb5e35352183b00f887897db26349cce324e965
ChristianSteen-SW23 <cniel23@student.aau.dk>
2023-12-12 23:02:15 +0100

    Done

--- AOC2023-3/Program.cs
+++ AOC2023-3/Program.cs
@@ -11,53 +11,110 @@ namespace AOC20233
 
             string[] lines = ReadFile(filePath);
 
-            int counter = 0;
+            int counterPart1 = 0;
             for (int lineC = 0; lineC  < lines.Length; lineC ++)
             {
                 Console.WriteLine(lines[lineC]);
                 for (int index = 0; index < lines[lineC].Length; index++)
                 {
-                    Console.WriteLine(lineC+" "+index);
+                    //Console.WriteLine(lineC+" "+index);
                     if (!CheckForSpecialChar(lines[lineC][index])) continue;
 
                     if (CheckForNumberChar(lines[lineC-1][index]))
                     {
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index));
                     } else
                     {
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index-1));
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index+1));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index-1));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index+1));
                     }
 
                     if (CheckForNumberChar(lines[lineC + 1][index]))
                     {
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index));
                     }
                     else
                     {
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index - 1));
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index + 1));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index - 1));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index + 1));
                     }
 
                     if (CheckForNumberChar(lines[lineC][index - 1])){
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC, index-1));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC, index-1));
                     }
                     if (CheckForNumberChar(lines[lineC][index + 1]))
                     {
-                        counter += Convert.ToInt32(FindNumberFromChar(lines, lineC, index + 1));
+                        counterPart1 += Convert.ToInt32(FindNumberFromChar(lines, lineC, index + 1));
                     }
-
-                    
-
-
                     //string number = FindNumberFromChar(lines, lineC - 1, index - 1);
                     //Console.WriteLine(number);
                     //Console.Write(lines[lineC][index]);
                 }
             }
-            Console.WriteLine("\n\n"+counter);
+            Console.WriteLine("Part one answer "+counterPart1);
 
+            int counterPart2 = 0;
+            int counterPart3 = 0;
+            for (int lineC = 0; lineC < lines.Length; lineC++)
+            {
+                for (int index = 0; index < lines[lineC].Length; index++)
+                {
 
+                    if (!CheckForGear(lines[lineC][index])) continue;
+                    //Console.WriteLine(counterPart2);
+                    int[] num= { 0, 0, 0, 0, 0, 0, 0, 0};
+                    if (CheckForNumberChar(lines[lineC - 1][index]))
+                    {
+                        num[0] = Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index));
+                    }
+                    else
+                    {
+                        num[1] = Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index - 1));
+                        num[2] = Convert.ToInt32(FindNumberFromChar(lines, lineC - 1, index + 1));
+                    }
+
+                    if (CheckForNumberChar(lines[lineC + 1][index]))
+                    {
+                        num[3] = Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index));
+                    }
+                    else
+                    {
+                        num[4] = Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index - 1));
+                        num[5] = Convert.ToInt32(FindNumberFromChar(lines, lineC + 1, index + 1));
+                    }
+
+                    if (CheckForNumberChar(lines[lineC][index - 1]))
+                    {
+                        num[6] = Convert.ToInt32(FindNumberFromChar(lines, lineC, index - 1));
+                    }
+                    if (CheckForNumberChar(lines[lineC][index + 1]))
+                    {
+                        num[7] = Convert.ToInt32(FindNumberFromChar(lines, lineC, index + 1));
+                    }
+                    int counter = 1;
+                    int counterAmount = 0;
+                    for (int i = 0; i < 8; i++) 
+                    {
+                        if (num[i] != 0)
+                        {
+                            
+                            counterAmount++;
+                            counter *= num[i];
+                        }
+                    }
+                    
+                    
+                    if (counterAmount == 2)
+                    {
+                        counterPart3++;
+                        counterPart2 += counter;
+                    }
+                    //string number = FindNumberFromChar(lines, lineC - 1, index - 1);
+                    //Console.WriteLine(number);
+                    //Console.Write(lines[lineC][index]);
+                }
+            }
+            Console.WriteLine("Part two answer " + counterPart2+" "+counterPart3);
 
             Console.ReadLine();
         }
@@ -139,6 +196,18 @@ namespace AOC20233
                 case '7':
                 case '8':
                 case '9':
+                    return true;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+        }
+        static bool CheckForGear(char ch)
+        {
+            switch (ch)
+            {
+                case '*':
                     return true;
                     break;
                 default:
